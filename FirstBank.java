fd529c2b58559310cbc39e14285ea4acbe9ec572
Shahzaib Shaikh <112446199+Shahzaibshaikh12@users.noreply.github.com>
2022-09-03 19:22:03 -0400

    Banking Application

---
+++ FirstBank.java
@@ -0,0 +1,20 @@
+public class FirstBank{
+
+    public static void main(String[] args){
+
+
+
+Account tim = new Account("Shahzaib Shaikh", "S1998");
+tim.showMenu();
+
+Account shahzaib = new Account("Shahzaib", "B00002");
+shahzaib.showMenu();
+
+
+}
+
+}
+
+
+
+
